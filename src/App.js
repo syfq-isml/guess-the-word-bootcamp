1a2a09f33b71a004ae7a66ade4c9c1eefcc2ea6b
Syafiq Ismail <macamyesjer@gmail.com>
2023-04-03 20:42:34 +0800

    add form input validation & game state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import { getRandomWord } from "./utils.js";
 import "./App.css";
 
+function isAllLetters(inputtxt) {
+    var letters = /^[A-Za-z]+$/;
+    if (inputtxt.match(letters)) return true;
+    return false;
+}
+
 class App extends React.Component {
     constructor(props) {
         // Always call super with props in constructor to initialise parent class
@@ -12,9 +18,12 @@ class App extends React.Component {
             // guessedLetters stores all letters a user has guessed so far
             guessedLetters: [],
 
-            guessesLeft: 10,
+            correctGuesses: 0,
+
+            guessesLeft: 6,
             guessInput: "",
             error: false,
+            errorMsg: "",
         };
     }
 
@@ -28,24 +37,53 @@ class App extends React.Component {
                 wordDisplay.push("_");
             }
         }
-        return wordDisplay.toString();
+        return {
+            wordWithSpace: wordDisplay.join(" "),
+            wordWithoutSpace: wordDisplay.join(""),
+        };
     };
 
     // Insert form callback functions handleChange and handleSubmit here
 
     handleSubmit = (e) => {
         e.preventDefault();
-        this.setState((prevState) => {
-            return {
-                ...prevState,
-                guessedLetters: [
-                    ...prevState.guessedLetters,
-                    prevState.guessInput,
-                ],
-                guessInput: "",
-                guessesLeft: prevState.guessesLeft - 1,
-            };
-        });
+        console.log(e);
+
+        let input = e.target[0].value;
+
+        if (!isAllLetters(input)) {
+            this.setState((prevState) => {
+                return {
+                    ...prevState,
+                    error: true,
+                    errorMsg: "Only alphabets pls",
+                };
+            });
+        } else {
+            this.setState((prevState) => {
+                if (prevState.currWord.includes(prevState.guessInput)) {
+                    return {
+                        ...prevState,
+                        correctGuesses: prevState.correctGuesses + 1,
+                        guessedLetters: [
+                            ...prevState.guessedLetters,
+                            prevState.guessInput,
+                        ],
+                        guessInput: "",
+                    };
+                }
+
+                return {
+                    ...prevState,
+                    guessedLetters: [
+                        ...prevState.guessedLetters,
+                        prevState.guessInput,
+                    ],
+                    guessInput: "",
+                    guessesLeft: prevState.guessesLeft - 1,
+                };
+            });
+        }
     };
 
     handleChange = (e) => {
@@ -55,8 +93,17 @@ class App extends React.Component {
                 return {
                     ...prevState,
                     error: true,
+                    errorMsg: "Only 1 character pls",
                 };
-            } else {
+            }
+            // else if (!isAllLetters(input) || input === "") {
+            //     return {
+            //         ...prevState,
+            //         error: true,
+            //         errorMsg: "Only alphabets pls",
+            //     };
+            // }
+            else {
                 return {
                     ...prevState,
                     guessInput: e.target.value,
@@ -67,25 +114,51 @@ class App extends React.Component {
     };
 
     render() {
+        let { wordWithSpace, wordWithoutSpace } = this.generateWordDisplay();
+
+        let gameResult;
+        if (wordWithoutSpace === this.state.currWord) gameResult = "win";
+        else if (this.state.guessesLeft === 0) gameResult = "lose";
         return (
             <div className="App">
                 <header className="App-header">
                     <h1>Guess The Word 🚀</h1>
                     <h3>Word Display</h3>
-                    {this.generateWordDisplay()}
+                    {wordWithSpace}
                     <h3>Guessed Letters</h3>
                     {this.state.guessedLetters.length > 0
-                        ? this.state.guessedLetters.toString()
+                        ? this.state.guessedLetters.join(", ")
                         : "-"}
                     <h3>Input</h3>
-                    <form onSubmit={this.handleSubmit}>
+                    <form autoComplete="off" onSubmit={this.handleSubmit}>
                         <input
+                            required
                             name="guessInput"
                             value={this.state.guessInput}
                             onChange={this.handleChange}
+                            disabled={
+                                gameResult === "lose"
+                                    ? true
+                                    : gameResult === "win"
+                                    ? true
+                                    : false
+                            }
                         />
-                        <button type="submit">Guess!</button>
-                        {this.state.error && <h3>Bro pls....</h3>}
+                        <button
+                            type="submit"
+                            disabled={
+                                gameResult === "lose"
+                                    ? true
+                                    : gameResult === "win"
+                                    ? true
+                                    : false
+                            }
+                        >
+                            Guess!
+                        </button>
+                        {this.state.error && <h3>{this.state.errorMsg}</h3>}
+                        {gameResult === "lose" && <h3>Game over...</h3>}
+                        {gameResult === "win" && <h3>You win!</h3>}
                     </form>
                 </header>
             </div>
