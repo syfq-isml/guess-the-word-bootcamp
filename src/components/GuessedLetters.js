e88a3dc97bc8775d83b82650f4aac20feacfa31c
Syafiq Ismail <macamyesjer@gmail.com>
2023-04-04 18:52:27 +0800

    add styling to guessed letters

--- src/components/GuessedLetters.js
+++ src/components/GuessedLetters.js
@@ -1,15 +1,53 @@
-import { Grid, Paper } from "@mui/material";
+import { Container, Grid, Paper, Typography } from "@mui/material";
 import React, { Component } from "react";
 
 class GuessedLetters extends Component {
     render() {
         return (
             <Grid item xs={4}>
-                <Paper>
-                    <h3>Guessed Letters</h3>
-                    {this.props.guessedLetters.length > 0
+                <Paper
+                    sx={{
+                        backgroundColor: "#B5EBC6",
+                        height: "100%",
+                        color: "#49443a",
+                    }}
+                >
+                    <Typography
+                        variant="h4"
+                        component="h4"
+                        p={1}
+                        sx={{ fontWeight: "700" }}
+                    >
+                        Guessed Letters
+                    </Typography>
+                    <Container
+                        sx={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}
+                    >
+                        {this.props.guessedLetters.sort().map((letter) => {
+                            return (
+                                <Paper
+                                    sx={{
+                                        width: "30px",
+                                        height: "30px",
+                                        display: "flex",
+                                        alignItems: "center",
+                                        justifyContent: "center",
+                                        padding: "20px",
+                                    }}
+                                >
+                                    <Typography
+                                        sx={{ fontWeight: "700" }}
+                                        variant="h4"
+                                    >
+                                        {letter}
+                                    </Typography>
+                                </Paper>
+                            );
+                        })}
+                    </Container>
+                    {/* {this.props.guessedLetters.length > 0
                         ? this.props.guessedLetters.join(", ")
-                        : "-"}
+                        : "-"} */}
                 </Paper>
             </Grid>
         );
